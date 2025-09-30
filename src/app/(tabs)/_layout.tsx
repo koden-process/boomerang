f1f56550273642e819a85f6b215323a5c118f512
Killian KOPP <killiankopp@gmail.com>
2025-09-30 15:34:00 +0200

    Update tab titles and icons in layout; modify welcome message in HomeScreen

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -19,15 +19,15 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          title: 'Tools',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="screwdriver.fill" color={color} />,
         }}
       />
       <Tabs.Screen
         name="explore"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: 'Loan',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="arrow.left.arrow.right" color={color} />,
         }}
       />
     </Tabs>
